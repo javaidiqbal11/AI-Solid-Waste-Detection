a50d5e1923313a6d3a7d0fc868611e27ee9e4f40
Javaid Iqbal <javaid.ciit@gmail.com>
2024-10-08 13:36:24 +0500

    Add files via upload

---
+++ Backend/app/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class User(BaseModel):
+    phone_number: str
+    device_id: str
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+
+class Data(BaseModel):
+    latitude: float
+    longitude: float
+    level: str
+
+class UpdateData(BaseModel):
+    id: str
+    annotations_str: Optional[str] = None
+
+class OTPRequest(BaseModel):
+    phone_number: str
+    otp: str
+
+class TokenData(BaseModel):
+    phone_number: Optional[str] = None
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
